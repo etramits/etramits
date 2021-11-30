3f72791b7922c3d20c229b286306ce1ce686de10
SEOAlexRamon <seoalexramon@gmail.com>
2021-11-30 18:44:08 +0100

    Added project base

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <Head :title="title" />
+  
+  <Header />
+  <slot></slot>
+</template>
+
+<script>
+  // Core
+  import { defineComponent } from 'vue'
+  import { Head, Link } from '@inertiajs/inertia-vue3'
+
+  // Components
+  import Header from '../Components/Header.vue'
+
+  export default defineComponent({
+    props: {
+      title: String,
+    },
+
+    components: {
+      Head,
+      Link,
+      Header,
+    },
+  })
+</script>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,120 @@
+<template>
+  <MainLayout title="Inici">
+    <!-- Slogan -->
+    <section class="bg-yellow-300	">
+      <div class="container flex flex-col md:flex-row items-center justify-between mx-auto px-4 py-10 text-gray-800">
+          <h2 class="text-4xl md:text-8xl font-bold" v-html="slogan"></h2>
+          <img class="mt-10 w-60 md:h-80 md:w-auto" src="/img/slogan.png">
+      </div>
+    </section>
+
+    <!-- Explore procedures -->
+    <section class="container flex flex-col items-center mx-auto my-12 md:my-24 text-gray-800">
+      <h3 class="text-3xl md:text-5xl font-medium">Explorar tràmits</h3>
+
+      <!-- Procedure -->
+      <div class="grid md:grid-cols-5 gap-10 mt-8 md:mt-14">
+        <div v-for="category in categories" :key="category.id" class="flex flex-col items-center p-6 bg-gray-100 rounded-md shadow hover:opacity-60 cursor-pointer">
+          <span class="mb-6 text-lg text-gray-400">12 tràmits</span>
+          <FontAwesomeIcon v-bind:icon="category.icon" size="5x" class="text-yellow-300" />
+          <h4 class="mt-6 text-xl md:text-2xl">{{ category.name }}</h4>
+        </div>
+      </div>
+    </section>
+
+    <!-- Stats -->
+    <div class="bg-gray-800 text-white">
+      <div class="container flex flex-col items-center mx-auto py-20">
+        <div class="grid md:grid-cols-3 gap-10">
+          <div v-for="registry in stats" :key="registry.id" class="flex flex-col items-center p-4 bg-gray-700 rounded-md shadow">
+            <div class="text-5xl font-semibold">{{ registry.value }}</div>
+            <div class="mt-1 text-xl md:text-2xl">{{ registry.label }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </MainLayout>
+</template>
+
+<script>
+  // Core
+  import { defineComponent } from 'vue'
+  import { Link } from '@inertiajs/inertia-vue3'
+
+  // Packages
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  import { fas } from '@fortawesome/free-solid-svg-icons'  
+  // Layouts
+  import MainLayout from '../Layouts/MainLayout.vue'
+
+  library.add(fas)
+
+  export default defineComponent({
+    components: {
+      Link,
+      MainLayout,
+      FontAwesomeIcon
+    },
+
+    props: {
+      slogan: String,
+    },
+
+    data() {
+      return {
+        categories: [
+          {
+            id: 0,
+            name: 'Salut',
+            icon: 'stethoscope',
+          },
+
+          {
+            id: 1,
+            name: 'Ocupació',
+            icon: 'briefcase',
+          },
+
+          {
+            id: 2,
+            name: 'Cites prèvies',
+            icon: 'receipt',
+          },
+
+          {
+            id: 3,
+            name: 'Habitatge',
+            icon: 'house-user',
+          },
+
+          {
+            id: 4,
+            name: 'Consell Comarcal',
+            icon: 'hands-helping',
+          },
+        ],
+
+        stats: [
+          {
+            id: 0,
+            label: 'Tràmits',
+            value: 320,
+          },
+
+          {
+            id: 0,
+            label: 'Comentaris',
+            value: 12,
+          },
+
+          {
+            id: 0,
+            label: 'Usuaris',
+            value: 8,
+          },
+        ]
+      }
+    }
+  })
+</script>
